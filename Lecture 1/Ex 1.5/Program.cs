ec784eb4ed9c6f11c276c6e64598c177d919dbb5
Sam Raumin <Samuel.raumin@gmail.com>
2021-01-26 18:11:05 -0800

    First Push, Initial

---
+++ Lecture 1/Ex 1.5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace First_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numCheck = 4.2; //this is the variable that can be changed to check
+
+            if(numCheck % 2 == 0)
+            {
+                Console.WriteLine("The integer is even");
+            }
+            else
+            {
+                Console.WriteLine("The integer is odd");
+            }
+        }
+        //changing the numCheck to a double does allow it to work, but the issue arises in decimals, as the given mod check does not support the possiblilty of a decimal place. 
+    }
+}
---
+++ Lecture 1/Ex 1.6/Ex1.6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace First_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numCheck = 4; //this is the variable that can be changed to check
+
+            if (numCheck % 2 == 0)
+            {
+                Console.WriteLine("The integer is even");
+            }
+            else
+            {
+                Console.WriteLine("The integer is odd");
+            }
+        }
+        //by using var as the type of the variable, the system implicitly assumes the type of numCheck based on what the initial cast determined by what is inputted, in this case being an integer
+    }
+}
