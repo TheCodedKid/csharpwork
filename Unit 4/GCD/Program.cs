b7042db9642aaa526e92dc142129f41ccf4f6edf
TheCodedKid <samuel.raumin@gmail.com>
2021-02-15 14:41:19 -0800

    GCD

---
+++ Unit 4/GCD/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GCD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static int brute(int m, int n, int g)
+        {
+            if (((m % g) == 0) && ((n % g) == 0))
+                return g;
+            else
+                return brute(m, n, g - 1);
+        }
+        static int euclid(int m, int n)
+        {
+            if ((m % n) == 0)
+                return n;
+            else
+                return euclid(n, m % n);
+        }
+        static int dijkstra(int m, int n)
+        {
+            if ((m % n) == 0)
+                return n;
+
+            else
+                return dijkstra(n, m % n);
+        }
+    }
+}
